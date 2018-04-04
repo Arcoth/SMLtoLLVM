69f648a1f25c5db0cbf3863a70cc038f2bcc26f6
Arcoth <r.hl@gmx.net>
2018-04-04 19:03:15 +0200

    Tidy up the code.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,8 @@
 
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/Verifier.h>
-#include <llvm/Support/TargetSelect.h>
-
-#include <llvm/Support/TargetRegistry.h>
-#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/FileSystem.h>
 
 #include <boost/process.hpp>
 
@@ -21,6 +19,8 @@ using namespace SMLCompiler;
 ImportsVector parseImports(std::istream& is) {
   for (std::string line; std::getline(is, line) && line != " the current import tree is :";)
     ;
+  if (!is.good())
+    throw std::runtime_error{"No import tree in stream!"};
 
   ImportsVector tree;
   for (std::string pid_label; (is >> pid_label) && pid_label == "Pid";) {
@@ -49,7 +49,6 @@ auto parseVerbosePlambda(std::istream& is) {
 
 void addSymbols(SMLTranslationUnit& unit) {
   auto& module = *unit.module;
-  auto& context = module.getContext();
   unit.symbolRepresentation[Symbol::symbol{Symbol::varInt, "::"}]
     = {.type = symbol_rep::UNTAGGED, .value = ConstantInt::getTrue(context)};
   unit.symbolRepresentation[Symbol::symbol{Symbol::varInt, "nil"}]
@@ -61,44 +60,12 @@ void addSymbols(SMLTranslationUnit& unit) {
     = {.type = symbol_rep::CONSTANT, .value = cast<ConstantInt>(ConstantInt::get(tagType(module), 0))};
 }
 
-
-
-int main(int argc, char** argv) try
-{
-  if (argc < 2) {
-    std::cout << "Pass a file to compile.";
-    return EXIT_SUCCESS;
-  }
-
-  namespace bp = boost::process;
-
-  bp::ipstream output;
-  bp::opstream input;
-  bp::child sml("smlnj/base/system/testml sml " + std::string{argv[1]},
-                bp::std_out > output, bp::std_in < input);
-  std::istream stream(new Parser::log_input_buf(output.rdbuf(), std::cout));
-
-  auto importMap = parseImports(stream);
-
-  // stream.exceptions(std::ios::failbit);
-  auto plambda = parseVerbosePlambda(stream);
-  if (!stream) {
-    std::cerr << "Failed to parse the plambda expression!";
-    return 0;
-  }
-
-  using namespace llvm;
-
-  LLVMContext context;
-  SMLTranslationUnit unit(plambda, std::move(importMap),
-                          new Module("SML module", context));
-  auto module = unit.module;
-  addSymbols(unit);
-
+void parseEntityData(SMLTranslationUnit& unit, std::istream& stream) {
   for (auto& [var, name] : unit.exportedDecls) {
     static auto skip_to = [&] (std::istream& s, char c) {
       s.ignore(std::numeric_limits<std::streamsize>::max(), c);
     };
+    bool name_already_parsed = false;
     for (;;) {
       auto kind = Parser::parse_alnum_id(stream);
       if (kind == "fun" || kind == "val")
@@ -113,64 +80,114 @@ int main(int argc, char** argv) try
           if (!ss)
             break;
           unit.symbolRepresentation[Symbol::symbol{Symbol::varInt, name}]
-            = {.type = symbol_rep::TAGGED, ConstantInt::get(tagType(*module), debruijn++)  };
+            = {.type = symbol_rep::TAGGED, ConstantInt::get(tagType(*unit.module), debruijn++)  };
           std::cout << "New constructor: " << name << '\n';
           skip_to(ss, '|');
         }
       }
+      else if (kind == "structure") {
+        name = Parser::parse_symbol_id(stream);
+        name_already_parsed = true;
+        for (std::string str; stream >> str && str != "end";)
+          ;
+        break;
+      }
       skip_to(stream, '\n');
     }
-    name = Parser::parse_symbol_id(stream);
+    if (!name_already_parsed)
+      name = Parser::parse_symbol_id(stream);
     skip_to(stream, '\n');
   }
+}
+
+SMLTranslationUnit parseSMLFile(std::string const& fileName) {
+  namespace bp = boost::process;
+
+  bp::ipstream output;
+  bp::opstream input;
+  bp::child sml("smlnj/base/system/testml sml " + fileName,
+                bp::std_out > output, bp::std_in < input);
+  std::istream stream(new Parser::log_input_buf(output.rdbuf(), std::cout));
+
+  auto importMap = parseImports(stream);
+
+  // stream.exceptions(std::ios::failbit);
+  auto plambda = parseVerbosePlambda(stream);
+  if (!stream)
+    throw std::runtime_error{"parseSMLFile: couldn't parse PLambda"};
+
+  using namespace llvm;
+
+  SMLTranslationUnit unit(plambda, std::move(importMap),
+                          new Module("SML module", context));
+
+  parseEntityData(unit, stream);
+
+  return unit;
+}
+
+template <typename Map>
+std::size_t getMainIndex(Map const& exportedDecls) {
+  std::set<std::string> functionNames;
+  for (auto& [_, n] : exportedDecls)
+    functionNames.insert(n);
+
+  auto decl_iter = functionNames.find("main");
+  if (decl_iter == functionNames.end())
+    throw CompileFailException{"No main function in SML program!"};
+
+  return std::distance(functionNames.begin(), decl_iter);
+}
+
+int main(int argc, char** argv) try
+{
+  if (argc < 2) {
+    std::cout << "Usage: SMLToLLVM <File>\n";
+    return EXIT_SUCCESS;
+  }
+
+  SMLTranslationUnit unit = parseSMLFile(argv[1]);
+  addSymbols(unit); // Add the core symbols (false, true, etc.)
+
+  auto module = unit.module;
 
   // Invoke the compiler.
   addGCSymbols(*module);
   SMLCompiler::compile_top(unit);
 
-  // Print out all of the generated code.
-  outs() << "\n\n\n\nPRINTING LLVM MODULE CONTENTS:\n\n";
-  module->print(outs(), nullptr);
-
   SMLCompiler::performOptimisationPasses(*module);
+
+  {
+    std::string str = argv[1];
+    str += ".llvm";
+    writeToFile(*module, str.c_str());
+  }
 
   if (verifyModule(*module, &errs())) {
     errs() << "\nThe code is ill-formed!\n\n";
     return EXIT_SUCCESS;
   }
 
-  SMLCompiler::performStatepointsPass(*module);
-
   std::cout << "Exporting " << unit.exportedDecls << '\n';
+
+  SMLCompiler::performStatepointsPass(*module);
 
   if (verifyModule(*module, &errs())) {
     errs() << "The code is ill-formed!";
     return EXIT_SUCCESS;
   }
 
-  std::set<std::string> functionNames;
-  for (auto& [_, n] : unit.exportedDecls)
-    functionNames.insert(n);
-
-  auto decl_iter = functionNames.find("main");
-  if (decl_iter == functionNames.end()) {
-    std::cerr << "\nNo main function in SML program!";
-    return EXIT_SUCCESS;
-  }
-
-  auto main_fun_index = std::distance(functionNames.begin(),
-                                      decl_iter);
-
-  return execute(main_fun_index, unit);
+  // Execute the main function
+  return execute(getMainIndex(unit.exportedDecls), unit);
 }
   catch (SMLCompiler::CompileFailException const& e) {
-  std::cerr << "Compilation failed: " << e.what();
+  std::cerr << "\n\nCompilation failed: " << e.what();
 } catch (SMLCompiler::UnsupportedException const& e) {
-  std::cerr << "Unsupported: " << e.what();
+  std::cerr << "\n\nUnsupported feature necessitated: " << e.what();
 } catch (std::exception const& e) {
-  std::cerr << "Exception: " << e.what();
+  std::cerr << "\n\nException: " << e.what();
   return EXIT_FAILURE;
 } catch (...) {
-  std::cerr << "An unknown exception interrupted the program.";
+  std::cerr << "\n\nAn unknown exception interrupted the program.";
   return EXIT_FAILURE;
 }
